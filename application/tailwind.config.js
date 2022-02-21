eb08c526bf0a68f837c74047da1335f72b28ea16
Anthony Merlier <46730440+ideosconsor@users.noreply.github.com>
2022-02-21 13:52:00 +0100

    Tailwind Istall ok

---
+++ application/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: [
+    './resources/views/**/*.edge',
+    './resources/css/**/*.css',
+    './resources/js/**/*.js',
+    './resources/js/**/*.ts',
+  ],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
