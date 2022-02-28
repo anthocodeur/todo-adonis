fa0679aaf3b8a6d2235971ffcebd783867f1694d
Anthony Merlier <46730440+ideosconsor@users.noreply.github.com>
2022-02-28 09:59:28 +0100

    edit docker and add models and migrations

--- application/start/routes.ts
+++ application/start/routes.ts
@@ -21,7 +21,5 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.get('/', async ({ view }) => view.render('home'))
-Route.get('/products', 'ProductsController.index')
-Route.get('/product/1', 'ProductsController.show')
 Route.get('/about', async ({ view }) => view.render('pages.about'))
 Route.get('/contact', async ({ view }) => view.render('pages.contact'))
