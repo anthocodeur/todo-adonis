8c3ad28c02121cae153e52ec45f9507785ca4c58
Anthony Merlier <46730440+ideosconsor@users.noreply.github.com>
2022-03-18 12:42:58 +0100

    forms ok + video 15 upload

--- application/app/Controllers/Http/TodosController.ts
+++ application/app/Controllers/Http/TodosController.ts
@@ -10,12 +10,17 @@ export default class TodosController {
     })
   }
 
-  public async create({}: HttpContextContract) {}
+  public async create({}: HttpContextContract) {
+    return View.render('todos.create')
+  }
 
-  public async store({ request }: HttpContextContract) {
+  public async store({ request, response }: HttpContextContract) {
+    //return request
     const todo = new Todo()
     todo.name = request.input('name')
+    todo.description = request.input('description')
     await todo.save()
+    response.redirect().toRoute('todos.show', [todo.id])
   }
 
   public async show({ params }: HttpContextContract) {
@@ -25,9 +30,14 @@ export default class TodosController {
     })
   }
 
-  public async edit({}: HttpContextContract) {}
+  public async edit({ params: { id } }: HttpContextContract) {
+    const todo = await Todo.findOrFail(id)
+    return View.render('todos.edit', {
+      todo,
+    })
+  }
 
-  public async update({ params: { id }, request }: HttpContextContract) {
+  public async update({ params: { id }, request, response }: HttpContextContract) {
     const todo = await Todo.findOrFail(id)
     const data = request.body()
     todo
@@ -35,6 +45,7 @@ export default class TodosController {
         ...data,
       })
       .save()
+    response.redirect().toRoute('todos.show', [todo.id])
   }
 
   public async destroy({ params }: HttpContextContract) {
--- application/start/todo/todos.ts
+++ application/start/todo/todos.ts
@@ -1,7 +1,12 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.get('/todos', 'TodosController.index').as('todos.index')
+
+Route.get('/todos/create', 'TodosController.create').as('todos.create')
+Route.post('/todos/create', 'TodosController.store').as('todos.store')
+
+Route.get('/todos/edit/:id', 'TodosController.edit').as('todos.edit')
+Route.post('/todos/edit/:id', 'TodosController.update').as('todos.update')
+
 Route.get('/todo/:id', 'TodosController.show').as('todos.show')
-Route.post('/todo', 'TodosController.store').as('todos.store')
-Route.patch('/todo/:id', 'TodosController.update').as('todos.update')
 Route.delete('/todo/:id', 'TodosController.destroy').as('todos.destroy')
